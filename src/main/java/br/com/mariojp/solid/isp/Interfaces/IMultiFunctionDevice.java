8fee225953096efed7cdd7c3a7c9e30a24fb737e
madduleal <mariaeduarda.leal@ucsal.edu.br>
2025-09-15 19:07:23 -0300

    refatoração da classe SimplePrinter

--- src/main/java/br/com/mariojp/solid/isp/Interfaces/IMultiFunctionDevice.java
+++
@@ -1,7 +0,0 @@
-package br.com.mariojp.solid.isp.Interfaces;
-
-public interface IMultiFunctionDevice {
-	void print(String content);
-
-	void scan(String targetFile);
-}
--- src/main/java/br/com/mariojp/solid/isp/SimplePrinter.java
+++
@@ -1,8 +0,0 @@
-package br.com.mariojp.solid.isp;
-
-import br.com.mariojp.solid.isp.Interfaces.IMultiFunctionDevice;
-
-public class SimplePrinter implements IMultiFunctionDevice {
-    @Override public void print(String content){ /* imprime */ }
-    @Override public void scan(String targetFile){ throw new UnsupportedOperationException("Sem scanner"); }
-}
