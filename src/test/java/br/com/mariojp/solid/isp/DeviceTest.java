1d891de736767f0e271cac543ba9524f85213417
madduleal <mariaeduarda.leal@ucsal.edu.br>
2025-09-15 19:15:54 -0300

    testes e resolução de tratativa em DeviceTest

--- src/test/java/br/com/mariojp/solid/isp/DeviceTest.java
+++ src/test/java/br/com/mariojp/solid/isp/DeviceTest.java
@@ -1,14 +1,15 @@
 package br.com.mariojp.solid.isp;
 
-import br.com.mariojp.solid.isp.Services.OfficeService;
 import org.junit.jupiter.api.Test;
+import br.com.mariojp.solid.isp.Models.SimplePrinter;
+import br.com.mariojp.solid.isp.Services.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DeviceTest {
     @Test
     void print_should_not_require_scan() {
         var svc = new OfficeService(new SimplePrinter());
-        assertDoesNotThrow(() -> svc.printReport("Relatório"),
+        assertDoesNotThrow(() -> svc.Print("Relatório"),
                 "Após refatoração, não deve chamar scan para imprimir.");
     }
 }
